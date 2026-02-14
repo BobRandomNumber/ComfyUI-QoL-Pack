a92ba72b536f0be83679d44737bb88574a029010
BobRandomNumber <189914665+BobRandomNumber@users.noreply.github.com>
2026-02-13 23:37:42 -0500

    v3 schema

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
-NODE_CLASS_MAPPINGS = {}
+from comfy_api.latest import ComfyExtension, io
+
+class QoLPackExtension(ComfyExtension):
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return []
+
+async def comfy_entrypoint() -> QoLPackExtension:
+    return QoLPackExtension()
 
 WEB_DIRECTORY = "./web"
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'WEB_DIRECTORY']
+__all__ = ["comfy_entrypoint", "WEB_DIRECTORY"]
